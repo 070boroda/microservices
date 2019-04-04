781455009b0cea358f45f03c7dec672f34736453
070boroda <zely1984@gmail.com>
2019-04-04 22:16:36 +0300

    rest + core + gateway is work

--- core/src/main/java/com/zelenko/entity/Teacher.java
+++ core/src/main/java/com/zelenko/entity/Teacher.java
@@ -1,9 +1,11 @@
 package com.zelenko.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 import javax.persistence.*;
 
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Teacher {
 
